c235643d2e30a71988edef108e01fd26c8a93cdf
Jihyeon Kim <simnalamburt@gmail.com>
2025-07-04 04:32:05 +0900

    Unusedlib.lean: Separate

--- LeanPractice/Obviouslib.lean
+++ LeanPractice/Obviouslib.lean
@@ -18,36 +18,3 @@ lemma rat_odd_denom_add {a b : ℚ} (ha : ¬(2 ∣ a.den)) (hb : ¬(2 ∣ b.den)
     simp_all [Rat.add_def, Rat.normalize, div_gcd_dvd (a.den * b.den) (a.num * ↑b.den + b.num * ↑a.den).natAbs]
   exact two_dvd_trans_contrapose hx hy
 
---
--- Series of unused lemmas
---
-lemma lt2_is_0or1 (a : ℕ) : a < 2 ↔ a = 0 ∨ a = 1 := by
-  have ltor : a < 2 → a = 0 ∨ a = 1 := by
-    intro h
-    cases h with
-    | refl => decide
-    | step => simp_all
-  exact ⟨ltor, by aesop⟩
-
-lemma nat_is_even_or_odd (a : ℕ) : (a % 2 = 0) ∨ (a % 2 = 1) := by
-  rw [← lt2_is_0or1]
-  exact Nat.mod_lt a (by decide)
-
-lemma nat_is_2dvd_or_not (a : ℕ) : (2 ∣ a) ∨ ¬(2 ∣ a) := by
-  have h : (a % 2 = 0) ∨ (a % 2 = 1) := by apply nat_is_even_or_odd
-  cases h with
-  | inl even => left; exact Nat.dvd_of_mod_eq_zero even
-  | inr odd => simp_all
-
-lemma even_is_2dvd (a : ℕ) : (a % 2 = 0) ↔ 2 ∣ a := by
-  have ltor : a % 2 = 0 → 2 ∣ a := by
-    intro h
-    apply Nat.dvd_of_mod_eq_zero
-    exact h
-  have rtol : 2 ∣ a → a % 2 = 0 := by
-    intro h
-    apply Nat.mod_eq_zero_of_dvd
-    exact h
-  exact ⟨ltor, rtol⟩
-
-lemma odd_is_not_2dvd (a : ℕ) : (a % 2 = 1) ↔ ¬(2 ∣ a) := by simp
