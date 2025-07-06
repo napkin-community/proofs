1ed1cfb0e7b2736f55646e5495cc29ebe6d0c605
Jihyeon Kim <simnalamburt@gmail.com>
2025-07-06 14:02:26 +0900

    Chapter11.lean: Create

--- LeanPractice.lean
+++ LeanPractice.lean
@@ -1,3 +1,4 @@
 -- This module serves as the root of the `LeanPractice` library.
 -- Import modules here that should be built as part of the library.
 import LeanPractice.Chapter1
+import LeanPractice.Chapter11
