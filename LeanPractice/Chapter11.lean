ba2fec507411dac7b5646cdafdc49996d158274b
Jihyeon Kim <simnalamburt@gmail.com>
2025-07-06 19:49:10 +0900

    Chapter11.lean: Problem 11B solved

--- LeanPractice/Chapter11.lean
+++ LeanPractice/Chapter11.lean
@@ -22,33 +22,4 @@ theorem trace_tensor_product_eq_mul_trace
   (S : W →ₗ[𝕜] W)
 : -- Tr(T ⊗ S) = Tr(T) Tr(S)
   LinearMap.trace 𝕜 (TensorProduct 𝕜 V W) (TensorProduct.map T S) = LinearMap.trace 𝕜 V T * LinearMap.trace 𝕜 W S
-:= by
-  let VW := TensorProduct 𝕜 V W
-  let TS := TensorProduct.map T S
-
-  let B_V := Module.finBasis 𝕜 V
-  let Tr_T := LinearMap.trace 𝕜 V T
-  let mat_T := LinearMap.toMatrix B_V B_V T
-  let Tr_mat_T := mat_T.trace
-  let h_T := LinearMap.trace_eq_matrix_trace 𝕜 B_V T
-  let h_T : Tr_T = Tr_mat_T := by aesop
-
-  let B_W := Module.finBasis 𝕜 W
-  let Tr_S := LinearMap.trace 𝕜 W S
-  let mat_S := LinearMap.toMatrix B_W B_W S
-  let Tr_mat_S := mat_S.trace
-  let h_S := LinearMap.trace_eq_matrix_trace 𝕜 B_W S
-  let h_S : Tr_S = Tr_mat_S := by aesop
-
-  let B_VW := Module.finBasis 𝕜 VW
-  let Tr_TS := LinearMap.trace 𝕜 VW TS
-  let mat_TS := LinearMap.toMatrix B_VW B_VW TS
-  let Tr_mat_TS := mat_TS.trace
-  let h_TS := LinearMap.trace_eq_matrix_trace 𝕜 B_VW TS
-  let h_TS : Tr_TS = Tr_mat_TS := by aesop
-
-  let h : Tr_mat_TS = Tr_mat_T * Tr_mat_S := by
-    sorry
-
-  rw [← h_T, ← h_S, ← h_TS] at h
-  exact h
+:= LinearMap.trace_tensorProduct' T S
