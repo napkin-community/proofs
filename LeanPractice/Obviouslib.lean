117c4e66619fc7014b17aa5971b205acdde983c9
Jihyeon Kim <simnalamburt@gmail.com>
2025-07-10 04:32:33 +0900

    Obviouslib.lean: Update

--- LeanPractice/Obviouslib.lean
+++ LeanPractice/Obviouslib.lean
@@ -1,4 +1,9 @@
 import Mathlib.Tactic.Ring
+import Mathlib.Data.Nat.Prime.Basic
+import Mathlib.Data.ZMod.Basic
+
+import Mathlib.Tactic.Ring
+import Mathlib.Tactic.Linarith
 
 /-
 # Series of obvious lemmas
@@ -6,6 +11,78 @@ import Mathlib.Tactic.Ring
 These lemmas are all very obvious, so I won't care too much about the
 readability of the proofs.
 -/
+private lemma if_dvd_nz_then_le
+  {a b : ℕ}
+  (h : a ∣ b)
+  (hz : b ≠ 0)
+: a ≤ b := by
+  rcases h with ⟨k, h⟩
+  rw [le_iff_exists_add]
+  use a*(k - 1)
+  cases k with
+  | zero => contradiction
+  | succ k =>
+    simp
+    linarith
+
+private lemma prime_is_coprime_with_smaller_pnats
+  {z p: ℕ}
+  (hz: z < p)
+  (hp: p.Prime)
+: z.Coprime p ↔ z ≠ 0 where
+  mp := by aesop
+  mpr h := by
+    have : ¬p ∣ z := by
+      intro hx
+      exact Nat.not_lt_of_ge (if_dvd_nz_then_le hx h) hz
+    have := Nat.Prime.coprime_pow_of_not_dvd (m := 1) hp this
+    aesop
+
+private def fin_coprime_equiv_fin_nz
+  {p : ℕ}
+  [Fact p.Prime]
+: { x : Fin p // x.val.Coprime p } ≃ { x : Fin p // x.val ≠ 0 } where
+  toFun x := by
+    have := prime_is_coprime_with_smaller_pnats (x.val.isLt) Fact.out
+    exact ⟨x.val, by aesop⟩
+  invFun x := by
+    have := prime_is_coprime_with_smaller_pnats (x.val.isLt) Fact.out
+    exact ⟨x.val, by aesop⟩
+  left_inv x := by aesop
+  right_inv x := by aesop
+
+private def zmod_coprime_equiv_fin
+  {p : ℕ}
+  [NeZero p]
+: { x : ZMod p // x.val.Coprime p } ≃ { x : Fin p // x.val.Coprime p } where
+  toFun x := by
+    have : p ≠ 0 := by aesop
+    cases p with
+    | zero => contradiction
+    | succ => exact x
+  invFun x := by
+    have : p ≠ 0 := by aesop
+    cases p with
+    | zero => contradiction
+    | succ => exact x
+  left_inv x := by
+    have : p ≠ 0 := by aesop
+    cases p with
+    | zero => contradiction
+    | succ => aesop
+  right_inv x := by
+    have : p ≠ 0 := by aesop
+    cases p with
+    | zero => contradiction
+    | succ => aesop
+
+def zmod_coprime_equiv_fin_nz
+  {p : ℕ}
+  [NeZero p]
+  [Fact p.Prime]
+: { x : ZMod p // x.val.Coprime p } ≃ { x : Fin p // x.val ≠ 0 } := by
+  exact Equiv.trans zmod_coprime_equiv_fin fin_coprime_equiv_fin_nz
+
 private lemma div_gcd_dvd (a b : ℕ) : a / b.gcd a ∣ a := by
   use b.gcd a
   calc
@@ -67,9 +144,24 @@ private lemma odd_mul2_mod4_eq2 {a : ℤ} (h : a % 2 = 1) : (a * 2) % 4 = 2 := b
 
 private lemma denom2rat_fact {a : ℚ} (h : a.den = 2) : (a.num * 2) % 4 = 2 := by
   have h := a.reduced
-  have h : ¬(2 ∣ a.num.natAbs) := by simp_all [odd_iff_coprime2]; exact h
+  have h : ¬(2 ∣ a.num.natAbs) := by have := @odd_iff_coprime2 a.num.natAbs; aesop
   have h : a.num % 2 = 1 := by simp_all [odd_iff_abs_odd]
   exact odd_mul2_mod4_eq2 h
+
+private lemma rat_add
+  {a b : ℚ}
+: (a + b).den = a.den * b.den / (a.num * b.den + b.num * a.den).natAbs.gcd (a.den * b.den) := by
+  simp [Rat.add_def, Rat.normalize]
+
+-- TODO
+private lemma rat1_add_rat2_den {a b : ℚ} (ha : a.den = 1) (hb : b.den = 2) : (a + b).den = 2 := by
+  have := b.reduced
+  have : b.num.gcd 2 = 1 := by simp_all only; exact this
+  have : (a.num * 2 + b.num).gcd 2 = 1 := by aesop
+  have : (a.num * 2 + b.num).natAbs.Coprime 2 := this
+  rw [rat_add]
+  simp_all only [dvd_mul_left, Int.gcd_add_left_left_of_dvd, one_mul, Nat.cast_ofNat, Nat.cast_one,
+    mul_one, Nat.div_one]
 
 theorem rat_denom_le2_add {a b : ℚ} (ha : a.den ≤ 2) (hb : b.den ≤ 2) : (a + b).den ≤ 2 := by
   have ha : a.den = 1 ∨ a.den = 2 := nz_le2_is_0or1 a.den_nz ha
@@ -78,23 +170,15 @@ theorem rat_denom_le2_add {a b : ℚ} (ha : a.den ≤ 2) (hb : b.den ≤ 2) : (a
   rcases ha with (ha | ha) <;> rcases hb with (hb | hb)
 
   -- case (1, 1)
-  have h : (a + b).den = 1 := by simp [Rat.add_def, Rat.normalize]; simp [ha, hb]
-  rw [h]
-  decide
+  have h : (a + b).den = 1 := by rw [rat_add]; aesop
+  aesop
 
   -- case (1, 2)
-  have h := b.reduced
-  have h : (a.num * 2 + b.num).gcd 2 = 1 := by aesop
-  have h : (a.num * 2 + b.num).natAbs.gcd 2 = 1 := h
-  have h : (a + b).den = 2 := by simp_all [Rat.add_def, Rat.normalize]
-  simp_all
+  simp [rat1_add_rat2_den ha hb]
 
   -- case (2, 1)
-  have h := a.reduced
-  have h : (a.num + b.num * 2).gcd 2 = 1 := by aesop
-  have h : (a.num + b.num * 2).natAbs.gcd 2 = 1 := h
-  have h : (a + b).den = 2 := by simp_all [Rat.add_def, Rat.normalize]
-  simp_all
+  rw [add_comm]
+  simp [rat1_add_rat2_den hb ha]
 
   -- case (2, 2)
   have h : (a.num * 2 + b.num * 2) % 4 = 0 := by
@@ -102,14 +186,13 @@ theorem rat_denom_le2_add {a b : ℚ} (ha : a.den ≤ 2) (hb : b.den ≤ 2) : (a
     decide
   have h : 4 ∣ (a.num * 2 + b.num * 2) := Int.dvd_of_emod_eq_zero h
   have h : ↑((a.num * 2 + b.num * 2).gcd 4) = (4 : ℤ) := by
-    have hx := @Int.gcd_eq_right_iff_dvd 4 (a.num * 2 + b.num * 2) (by decide)
-    simp_all [hx]
+    simp_all [@Int.gcd_eq_right_iff_dvd 4 (a.num * 2 + b.num * 2) (by decide)]
   have h : (a.num * 2 + b.num * 2).gcd 4 = 4 := by
     rw [← (@Nat.cast_inj ℤ)]
     exact h
   have h : (a.num * 2 + b.num * 2).natAbs.gcd 4 = 4 := h
-  have h : (a + b).den = 1 := by simp_all [Rat.add_def, Rat.normalize]
-  simp_all
+  have h : (a + b).den = 1 := by rw [rat_add]; aesop
+  aesop
 
 theorem rat_denom_le2_mul : ¬(∀ {a b : ℚ}, a.den ≤ 2 → b.den ≤ 2 → (a * b).den ≤ 2) := by
   intro h
