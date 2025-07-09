e0b8e00ed38833ba7e1841547dbba30772bd5ce6
Jihyeon Kim <simnalamburt@gmail.com>
2025-07-10 06:55:04 +0900

    Chapter1.lean: Solve more examples

--- LeanPractice/Chapter1.lean
+++ LeanPractice/Chapter1.lean
@@ -1,6 +1,6 @@
-import Mathlib.LinearAlgebra.Matrix.NonsingularInverse
 import Mathlib.Analysis.Complex.Circle
 import Mathlib.LinearAlgebra.Matrix.GeneralLinearGroup.Defs
+import Mathlib.GroupTheory.SpecificGroups.Dihedral
 
 import LeanPractice.Obviouslib
 
@@ -45,7 +45,7 @@ example : ¬(∀ {a : ℚ}, a * a⁻¹ = 1) := by
 /-
 • The pair (Z, ·) is also NOT a group. (Why?)
 -/
-example (grp : Group ℤ) : ¬(∀ {a : ℤ}, a * a⁻¹ = 1) := by
+example [Group ℤ] : ¬(∀ {a : ℤ}, a * a⁻¹ = 1) := by
   push_neg
   use 0
   linarith
@@ -56,9 +56,7 @@ example (grp : Group ℤ) : ¬(∀ {a : ℤ}, a * a⁻¹ = 1) := by
 -/
 example : ¬(∀ {a : (Matrix (Fin 2) (Fin 2) ℝ)}, a * a⁻¹ = 1) := by
   intro h
-  let zero : (Matrix (Fin 2) (Fin 2) ℝ) := 0
-  specialize h (a := zero)
-  have : zero⁻¹ = zero := by simp [zero]
+  specialize h (a := 0)
   aesop
 
 /-
@@ -83,14 +81,17 @@ residues (remainders) modulo n. These form a group under addition. We call this
 the **cyclic group of order n**, and denote it as Z/nZ, with elements 0, 1, . . . . The
 identity is 0.
 -/
-instance (n : ℕ) : AddCommGroup (ZMod n) := by infer_instance
+instance : AddCommGroup (ZMod n) := by infer_instance
 
 /-
 # Example 1.1.9 (Multiplication mod p)
 Let p be a prime. Consider the *nonzero residues modulo* p, which we denote by
 (Z/pZ)^×. Then ((Z/pZ)^×, ×) is a group.
 -/
-instance (p : ℕ) [Fact p.Prime] : CommGroup (ZMod p)ˣ := by infer_instance
+instance [Fact p.Prime] : CommGroup (ZMod p)ˣ := by infer_instance
+example [Fact p.Prime] : Fintype.card (ZMod p)ˣ = p - 1 := by
+  rw [Fintype.card_congr ZMod.unitsEquivCoprime, Fintype.card_congr zmod_coprime_equiv_fin_nz]
+  aesop
 
 /-
 # Question 1.1.10. Why do we need the fact that p is prime?
@@ -98,8 +99,7 @@ instance (p : ℕ) [Fact p.Prime] : CommGroup (ZMod p)ˣ := by infer_instance
 example : ¬(∀ {n : ℕ} [NeZero n], Fintype.card (ZMod n)ˣ = n - 1) := by
   intro h
   specialize h (n := 4)
-  have : Fintype.card (ZMod 4)ˣ = 2 := by decide
-  tauto
+  contradiction
 
 /-
 # Example 1.1.11 (General linear group)
@@ -108,8 +108,7 @@ which have nonzero determinant. It turns out that with this condition, every mat
 does indeed have an inverse, so (GL_n(ℝ), ×) is a group, called the general linear
 group.
 -/
-instance (n : ℕ) [NeZero n] : Group (Matrix.GeneralLinearGroup (Fin n) ℝ) := by
-  infer_instance
+instance [NeZero n] : Group (Matrix.GeneralLinearGroup (Fin n) ℝ) := by infer_instance
 
 /-
 # Example 1.1.12 (Special linear group)
@@ -117,10 +116,61 @@ Following the example above, let SL_n(ℝ) denote the set of n × n matrices who
 determinant is actually 1. Again, for linear algebra reasons it turns out that
 (SL_n(ℝ), ×) is also a group, called the special linear group.
 -/
-instance (n : ℕ) [NeZero n] : Group (Matrix.SpecialLinearGroup (Fin n) ℝ) := by
-  infer_instance
+instance [NeZero n] : Group (Matrix.SpecialLinearGroup (Fin n) ℝ) := by infer_instance
 
--- TODO
+/-
+# Example 1.1.13 (Symmetric groups)
+Let S_n be the set of permutations of {1, ... , n}. By viewing these permutations as
+functions from {1, ... , n} to itself, we can consider *compositions* of permutations.
+Then the pair (S_n, ◦) (here ◦ is function composition) is also a group, because
+
+- There is an identity permutation, and
+- Each permutation has an inverse.
+
+The group S_n is called the **symmetric group** on n elements.
+-/
+instance : Group (Equiv.Perm (Fin n)) := by infer_instance
+
+/-
+# Example 1.1.14 (Dihedral group)
+The **dihedral group of order 2n**, denoted D_2n, is the group of symmetries of a
+regular n-gon A_1, A_2, ... , A_n, which includes rotations and reflections. It consists of
+the 2n elements
+
+    {1, r, r^2, ..., r^{n-1}, s, sr, sr^2, ..., sr^{n-1} }.
+
+The element r corresponds to rotating the n-gon by 2π/n, while s corresponds to
+reflecting it across the line OA_1 (here O is the center of the polygon). So rs means
+“reflect then rotate” (like with function composition, we read from right to left).
+In particular, r^n = s^2 = 1. You can also see that r^k s = sr^−k.
+-/
+instance : Group (DihedralGroup n) := by infer_instance
+
+/-
+# Example 1.1.15 (Products of groups)
+Let (G, ⋆) and (H, *) be groups. We can define a product group (G × H, ·), as
+follows. The elements of the group will be ordered pairs (g, h) ∈ G × H. Then
+
+    (g1, h1) · (g2, h2) = (g1 ⋆ g2, h1 * h2) ∈ G × H
+
+is the group operation.
+-/
+instance [Group A] [Group B] : Group (A × B) := by infer_instance
+
+/-
+# Question 1.1.16. What are the identity and inverses of the product group?
+-/
+example
+  {A B : Type} {a : A} {b : B}
+  [Group A] [Group B] [Group (A × B)]
+: (1 : (A × B)) = (1, 1) ∧ (a, b)⁻¹ = (a⁻¹, b⁻¹) := by aesop
+
+/-
+# Example 1.1.17 (Trivial group)
+The trivial group, often denoted 0 or 1, is the group with only an identity element.
+I will use the notation {1}.
+-/
+instance : CommGroup PUnit := by infer_instance
 
 /-
 # Exercise 1.1.18. Which of these are groups?
@@ -168,7 +218,7 @@ instance : AddCommGroup RatDenomLE2 where
 /-
 (c) The set of rational numbers with denominator at most 2, where the operation is multiplication.
 -/
-example (grp : Group RatDenomLE2) : ¬(∀ {a b : RatDenomLE2}, (a * b).val = a.val * b.val) := by
+example [Group RatDenomLE2] : ¬(∀ {a b : RatDenomLE2}, (a * b).val = a.val * b.val) := by
   intro h
   have h0 := rat_denom_le2_mul
   push_neg at h0
@@ -182,7 +232,26 @@ example (grp : Group RatDenomLE2) : ¬(∀ {a b : RatDenomLE2}, (a * b).val = a.
 /-
 (d) The set of nonnegative integers, where the operation is addition.
 -/
-example (grp : AddGroup ℕ) : ¬(∀ {a : ℕ}, a + (-a) = 0) := by
+example [AddGroup ℕ] : ¬(∀ {a : ℕ}, a + (-a) = 0) := by
   intro h
   specialize h (a := 1)
   linarith
+
+/-
+# Proposition 1.2.4 (Inverse of products)
+Let G be a group, and a, b ∈ G. Then (ab)^−1 = b^−1a^−1
+-/
+example [Group G] (a b : G) : (a * b)⁻¹ = b⁻¹ * a⁻¹ := by aesop
+
+/-
+# Lemma 1.2.5 (Left multiplication is a bijection)
+Let G be a group, and pick a g ∈ G. Then the map G → G given by λx ↦ gx is a
+bijection.
+
+# Exercise 1.2.6.
+Check this by showing injectivity and surjectivity directly. (If you don’t know
+what these words mean, consult Appendix E.)
+-/
+example {G : Type} [Group G] (g : G) : Function.Bijective (λx ↦ g * x) where
+  left a b := by aesop
+  right y := by use g⁻¹ * y; aesop
