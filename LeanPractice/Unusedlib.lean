20056d1cbca6f8b0950bf6aa9fd6b02008d66e03
Jihyeon Kim <simnalamburt@gmail.com>
2025-07-10 04:30:26 +0900

    Unusedlib.lean: Update

--- LeanPractice/Unusedlib.lean
+++ LeanPractice/Unusedlib.lean
@@ -1,19 +1,23 @@
-import LeanPractice.Obviouslib
+import Aesop
+import Mathlib.Data.Nat.Notation
+import Mathlib.Data.Rat.Init
+import Mathlib.Algebra.Group.Nat.Even
+import Mathlib.Tactic.Lemma
 
 --
 -- Series of unused lemmas
 --
-lemma lt2_is_0or1 (a : ℕ) : a < 2 ↔ a = 0 ∨ a = 1 :=
+private lemma lt2_is_0or1 (a : ℕ) : a < 2 ↔ a = 0 ∨ a = 1 :=
   have ltor (h : a < 2) : a = 0 ∨ a = 1 := match a with | 0 | 1 => by decide
   have rtol (h : a = 0 ∨ a = 1) : a < 2 := match a with | 0 | 1 => by decide
   ⟨ltor, rtol⟩
 
-lemma nat_is_2dvd_or_not (a : ℕ) : (2 ∣ a) ∨ ¬(2 ∣ a) := by
+private lemma nat_is_2dvd_or_not (a : ℕ) : (2 ∣ a) ∨ ¬(2 ∣ a) := by
   cases (Nat.mod_two_eq_zero_or_one a) with
   | inl even => left; exact Nat.dvd_of_mod_eq_zero even
-  | inr odd => simp_all
+  | inr odd => aesop
 
-lemma even_is_2dvd (a : ℕ) : (a % 2 = 0) ↔ 2 ∣ a :=
+private lemma even_is_2dvd (a : ℕ) : (a % 2 = 0) ↔ 2 ∣ a :=
   have ltor : a % 2 = 0 → 2 ∣ a := by
     intro h
     apply Nat.dvd_of_mod_eq_zero
@@ -24,4 +28,11 @@ lemma even_is_2dvd (a : ℕ) : (a % 2 = 0) ↔ 2 ∣ a :=
     exact h
   ⟨ltor, rtol⟩
 
-lemma odd_is_not_2dvd (a : ℕ) : (a % 2 = 1) ↔ ¬(2 ∣ a) := by simp
+private lemma odd_is_not_2dvd (a : ℕ) : (a % 2 = 1) ↔ ¬(2 ∣ a) := by simp
+
+private lemma rat_eq_int
+  {a : ℚ}
+  {n : ℤ}
+  (h : a = n)
+: a.den = 1 ∧ a.num = n := by
+  aesop
